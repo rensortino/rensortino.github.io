622fba4905785b1a108267a4dff5560af4110510
Renato Sortino <rensortino@gmail.com>
2024-02-15 18:15:34 +0100

    Fixed dropdown events

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function toggleClass(element) {
+function toggleClass(element, target) {
     element.querySelector('i').classList.toggle("bi-caret-right-fill");
     element.querySelector('i').classList.toggle("bi-caret-down-fill");
+
+    console.log(element);
+    console.log(target);
+    
+    if (window.innerWidth > 768) {
+        target.classList.add("collapsible");
+        target.classList.remove("collapsed");
+    }
+    else {
+        target.classList.toggle("collapsible");
+        target.classList.toggle("collapsed");
+        // target.style.display = target.style.display === "none" ? "block" : "none";
+    
+    }
+
 }
 document.querySelector("#year").innerHTML = new Date().getFullYear();
